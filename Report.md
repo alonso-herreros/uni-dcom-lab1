fa32b6e88cdb2ae32b060ffa3c9c6ac2ef762c97
Alonso Herreros <alonso.herreros.c@gmail.com>
2024-11-10 20:04:42 +0100

    Add section 1

---
+++ Report.md
@@ -0,0 +1,63 @@
+---
+title: Lab 1 Report
+---
+
+<style>
+:root {
+    --markdown-font-family: "Times New Roman", Times, serif;
+    --markdown-font-size: 10.5pt;
+}
+</style>
+
+<p class="supt1 center">Communication Theory</p>
+
+# Lab 1 Report
+
+<p class="subt2 center">
+Academic year 2024-2025
+</p>
+<p class="subt2 center">
+Alonso Herreros Copete
+</p>
+
+---
+
+### Table of Contents
+
+* [Signal-to-Noise ratio](#signal-to-noise-ratio)
+
+---
+
+## Signal-to-Noise ratio
+
+As instructed, the dispersion diagram for the 16-QAM signal was created for
+different SNRs, including the corresponding values of $N_0$. The results can be
+found in the following figures.
+
+![alt](./figures/1.1.0-A.png)
+<p class="caption">
+Figure 1.1.0: Original 16-QAM signal.
+</p>
+
+![alt](./figures/1.1.1-snr20.png)
+<p class="caption">
+Figure 1.1.1: q[n] observed at SNR = 20 dB.
+</p>
+
+![alt](./figures/1.1.2-snr15.png)
+<p class="caption">
+Figure 1.1.1: q[n] observed at SNR = 15 dB.
+</p>
+
+![alt](./figures/1.1.3-snr10.png)
+<p class="caption">
+Figure 1.1.1: q[n] observed at SNR = 10 dB.
+</p>
+
+![alt](./figures/1.1.4-snr5.png)
+<p class="caption">
+Figure 1.1.1: q[n] observed at SNR = 5 dB.
+</p>
+
+As we can observe in the figures, as the SNR decreases, the signal becomes very
+hard to interpret.
---
+++ code/section1.m
@@ -0,0 +1,50 @@
+%-------------------------------------------------------------------------
+%
+% This file was created based on the demo file while carrying out the lab
+% exercise, following the lab guide.
+%
+% Updated by : Alonso Herreros <alonso.herreros.c@gmail.com>
+%       Date : november 2024
+%
+%-------------------------------------------------------------------------
+
+%% -- Basic parameters
+
+M = 16;                 % Constellation order
+m = log2(M);            % Bits per symbol
+nSimb = 1e6;            % Number of symbols in the simulation
+nBits = nSimb * m;      % Number of bits in the simulation
+tAssig = 'gray';        % Type of binary assignement ('gray', 'bin')
+SNR_dB = 25;            % S/N in dB
+Es = 10;                % Mean Energy per Symbol
+p=[1];                  % Equivalent discrete channel
+
+
+%% -- Digital QAM Modulator
+
+% Generation of Bits 
+B = randi([0 1], nBits, 1);
+% Symbols encoded from bits
+A = qammod(B, M, tAssig, InputType='bit'); 
+% Scatter diagram
+f = figure(1);
+scatterplot(A);title('Scatter plot of original A[n]');
+print('figures/1.1.0-A.png', '-dpng');
+
+
+%% -- Transmission through channel
+o = conv(A, p);
+o = o(1:nSimb);
+
+
+%% -- AWGN
+
+i = 1;
+for snr=[20 15 10 5]
+    % Additive White Gaussian Noise
+    q = awgn(o, snr, 10*log10(Es));
+    scatterplot(q);
+    title(sprintf('Scatter plot of q[n] for SNR=%d dB ($N_0 = %.3f$)', snr, Es * 10^(-snr/10)))
+    print(sprintf('figures/1.1.%d-snr%d.png', i, snr), '-dpng');
+    i = i+1;
+end
