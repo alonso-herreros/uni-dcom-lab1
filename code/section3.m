008433cab7cb56cbf9e08145050ae05aa56f496c
Alonso Herreros <alonso.herreros.c@gmail.com>
2024-11-13 11:43:21 +0100

    Finish section 3 code and plots

--- code/section3.m
+++ code/section3.m
@@ -24,9 +24,23 @@ set(groot, 'defaultLegendInterpreter', 'latex');
 % Es = 10;                % Mean Energy per Symbol
 % p=[1];                  % Equivalent discrete channel -- VAR
 
+%% Custom functions
+
+% Calculate symbol error rate based on bit errors and QAM order
+function SER = symerr(Berrs, M)
+    m = log2(M);
+    nSym = numel(Berrs)/m;
+
+    % Since qamdemod doesn't output estimated symbols, we'll use the fact that
+    % one or more bit errors in an aligned group of m=log2(M) bits is 1 symbol
+    % error
+    SER = sum(sum(reshape(Berrs, m, nSym)) ~= 0)/nSym;
+end
+
+
 %% Experiment definition
 
-function [BER_data, SER_data] = experiment(Ms, SNRs)
+function [BERs, SERs] = experiment(M, SNRs)
 
     % Static params
     persistent nSimb tAssig Es p;
@@ -36,92 +50,99 @@ function [BER_data, SER_data] = experiment(Ms, SNRs)
     p = [1];                % Equivalent discrete channel
 
     % Create original sequence A[n] and noiseless sequence o[n]
-    
-    % Get BERs for each M
-    BER_data = zeros(numel(Ms), numel(SNRs));
-    SER_data = zeros(numel(Ms), numel(SNRs));
-    for i=1:numel(Ms)
-        M = Ms(i);
-        [B, A, o] = experiment_init(M, nSimb, tAssig, p);
-        [BERs, SERs] = experiment_bers(M, tAssig, Es, A, B, o, SNRs);
-        BER_data(i,:) = BERs;
-        SER_data(i,:) = SERs;
+    [B, ~, o] = experiment_init(M, nSimb, tAssig, p);
+
+    % Get bit and symbol error rates
+    BERs = zeros(size(SNRs));
+    SERs = zeros(size(SNRs));
+    for i=1:numel(SNRs)
+        snr = SNRs(i);
+        % Get error rates
+        [~, ~, BERs(i), SERs(i)] = experiment_exec(M, tAssig, snr, Es, B, o);
     end
 end
 
 function [B, A, o] = experiment_init(M, nSimb, tAssig, p)
     m = log2(M);            % Bits per symbol
     nBits = nSimb * m;      % Number of bits in the simulation
+
     % Digital QAM Modulator
-    B = randi([0 1], nBits, 1); % Generation of Bits 
+    B = randi([0 1], nBits, 1);                % Generation of Bits 
     A = qammod(B, M, tAssig, InputType='bit'); % Symbols encoded from bits
+
     % Discrete equivalent channel
     o = conv(A, p); o = o(1:nSimb);
 end
 
-function [q, Be] = experiment_exec(M, tAssig, SNR_dB, Es, o)
-    q = awgn(o, SNR_dB, 10*log10(Es));
-    Be = qamdemod(q, M, tAssig, OutputType='bit');
-end
-
-function [BERs, SERs] = experiment_bers(M, tAssig, Es, A, B, o, snr_values)
-    m = log2(M);
+% Get noisy sequence, estimated bits, BER, and SER
+function [q, Be, BER, SER] = experiment_exec(M, tAssig, snr, Es, B, o)
+    % Get noisy sequence
+    q = awgn(o, snr, 10*log10(Es));
     
-    % Get error rates for each snr
-    BERs = zeros(size(snr_values));
-    SERs = zeros(size(snr_values));
-    for i=1:numel(snr_values)
-        snr = snr_values(i);
+    % Get demodulated bits
+    Be = qamdemod(q, M, tAssig, OutputType='bit');
 
-        % Get demodulated bits
-        [~, Be] = experiment_exec(M, tAssig, snr, Es, o);
-        
-        % Bit error rate
-        [~, BERs(i)] = biterr(B, Be);
-
-        % Symbol error rate. Since qamdemod doesn't output estimated
-        % symbols, we'll use the fact that a single bit error in an
-        % aligned group of m=log2(M) bits is a symbol error
-        Berrs = B~=Be;
-        symbol_errors = 0;
-        for j=0:numel(B)/m-1
-            if (sum(Berrs(m*j+1:m*(j+1))) ~= 0)
-                symbol_errors = symbol_errors +1;
-            end
-        end
-        SERs(i) = double(symbol_errors)/double(numel(A));
-    end
+    % Bit and symbol error rates
+    [~, BER, Berrs] = biterr(B, Be);
+    SER = symerr(Berrs, M);
 end
 
-%% Section 3.
 
+%% Section 3.1 - Running experiments
 section = [3 1];
-% Figure prefix
-fprefix = sprintf('../figures/%d.%d', section(1), section(2));
 
 % Experiment parameters
 Ms = [4 16 64];
 SNRs = 0:1:20;
 
-[BER_data, SER_data] = experiment(Ms, SNRs);
+% -- Run experiments
+BER_data = zeros(numel(Ms), numel(SNRs));
+SER_data = zeros(numel(Ms), numel(SNRs));
+for i=1:numel(Ms)
+    M = Ms(i);
+    fprintf('Running experiment for %d-QAM...\n',M);
+    [BER_data(i,:), SER_data(i,:)] = experiment(M, SNRs);
+end
+SER_BER_ratios = SER_data./BER_data;
 
-% Plots
+%% Section 3.1 - Plots
+fprintf('Plotting data...\n');
+
+% Figure prefix
+fprefix = sprintf('../figures/%d.%d', section(1), section(2));
+
+% BERs
 figure(1); clf; hold on;
 for i=1:numel(Ms)
     semilogy(SNRs, BER_data(i,:), DisplayName=sprintf('BERs for %d-QAM', Ms(i)));
-end
+end; hold off;
+grid on;
 title('Bit error probabilities for different modulations');
 xlabel('Signal-to-noise ratio (dB)');
 ylabel('Bit error rate (probability)'); yscale log;
 legend('show');
 print(sprintf('%s.1-BERs.png', fprefix), '-dpng');
  
+% SERs
 figure(2); clf; hold on;
 for i=1:numel(Ms)
     semilogy(SNRs, SER_data(i,:), DisplayName=sprintf('SERs for %d-QAM', Ms(i)));
-end
+end; hold off;
+grid on;
 title('Symbol error probabilities for different modulations');
 xlabel('Signal-to-noise ratio (dB)');
 ylabel('Symbol error rate (probability)'); yscale log;
 legend('show')
 print(sprintf('%s.2-SERs.png', fprefix), '-dpng');
+
+% SER-BER ratios
+figure(3); clf; hold on;
+for i=1:numel(Ms)
+    plot(SNRs, SER_BER_ratios(i,:), DisplayName=sprintf('SER/BER ratio for %d-QAM', Ms(i)))
+end; hold off;
+grid on;
+title('SER-BER ratios for different modulations');
+xlabel('Signal-to-noise ratio (dB)');
+ylabel('SER/BER (ratio)'); ylim([0, max(SER_BER_ratios, [], 'all')]);
+legend('show')
+print(sprintf('%s.3-SER-BER-ratios.png', fprefix), '-dpng');
